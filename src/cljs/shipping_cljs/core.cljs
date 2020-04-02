4085ac1f35d87d4fbc64c4d0bae4a8b6cc6fc7f3
Joe Creager <josephcreager@gmail.com>
2020-04-02 13:43:49 -0700

    move service worker to cljs and track installation

--- src/cljs/shipping_cljs/core.cljs
+++ src/cljs/shipping_cljs/core.cljs
@@ -32,6 +32,26 @@
  (fn [db _]
    (:install-prompt db)))
 
+(rf/reg-event-db
+ :service-worker
+ (fn [db [_ prompt]]
+   (assoc db :service-worker prompt)))
+
+(rf/reg-sub
+ :service-worker
+ (fn [db _]
+   (:service-worker db)))
+
+(rf/reg-event-db
+ :installed
+ (fn [db [_ installed]]
+   (assoc db :installed installed)))
+
+(rf/reg-sub
+ :installed
+ (fn [db _]
+   (:installed db)))
+
 (kf/reg-chain
   ::load-home-page
   (fn [_ _]
@@ -59,15 +79,25 @@
                 :initial-db     {}
                 :root-component [view/root-component]})))
 
-(defn install-prompt-listener []
+(defn register-service-worker []
+  (if (.-serviceWorker js/navigator)
+    (-> (js/navigator.serviceWorker.register "/service-worker.js")
+        (.then (rf/dispatch [:service-worker true]))
+        (.catch (rf/dispatch [:service-worker false])))))
+
+(defn register-install-prompt-listener []
   (js/window.addEventListener
    "beforeinstallprompt"
    (fn [e]
-     (do (js/console.log "hi")
-         (.preventDefault e)
+     (do (.preventDefault e)
          (rf/dispatch [:install-prompt e])))))
 
+(defn is-standalone? []
+  (or (.-standalone js/navigator) (.-matches (js/matchMedia "(display-mode: standalone"))))
+
 (defn init! [debug?]
-  (install-prompt-listener)
+  (rf/dispatch-sync [:installed false])
+  (register-service-worker)
+  (register-install-prompt-listener)
   (ajax/load-interceptors!)
   (mount-components debug?))
